4f35dd145c81e78b1ac00922fd6aa5822b655182
Robert Pollai <web@pollai.at>
2025-04-01 18:27:36 +0200

    Moved change of directory to tasks.json

--- source-annot-tinyglance/create-source-files.bat
+++ source-annot-tinyglance/create-source-files.bat
@@ -1,6 +1,6 @@
 REM Copies the source files from source-annot-glance
 REM and replaces the annotations. See README.MD for details.
-cd .\source-annot-tinyglance\
+REM cd .\source-annot-tinyglance\
 copy ..\source-annot-glance\*.mc .\
 sed -i "s/(:glance) //g" *.mc
 sed -i "s/(:glance :background) /(:background) /g" *.mc
